3bf1e51cf360bb5f97b8fe7ef7cabed2f6dc2f10
Krishna V <kg5300@srmist.edu.in>
2022-12-08 23:32:30 +0530

    Update index.js

--- Black_Jack/index.js
+++ Black_Jack/index.js
@@ -1,12 +1,21 @@
-let firstCard;
-let secondCard;
-let sum;
+let sum = 0;
 let age;
 let ageState = false;
 let handState = false;
 let isAlive = true;
 let message = "";
 let ageMsg = "";
+let cards = [];
+const messages = [
+    "Welcome to the Club. Enjoy your visit 😊", 
+    "We're sorry. You're not allowed to enter this club 🥲",
+    "Inavlid Input. Please try again",
+    "Congratulations, you won.",
+    "You've losen your hand.",
+    "You've got one more chance.",
+    " Click RESTART to play again",
+    "Enter your age to check your eligibility."
+]
 
 const ageObject = document.getElementById("age");
 const ageEl = document.getElementById("age-el");
@@ -15,66 +24,34 @@ const sumEl = document.getElementById("sum-el");
 const cardsEl = document.getElementById("cards-el");
 const start = document.getElementById('start-el');
 const newCardObj = document.getElementById("newCard");
+const restartObj = document.getElementById("restartGame");
 
-function sumCards(firstCard, secondCard){
-    sum = firstCard + secondCard;
-}
+messageEl.textContent = messages[7];
+sumEl.style.display = 'none';
+cardsEl.style.display = 'none';
+start.style.display = 'none';
+newCardObj.style.display = 'none';
+restartObj.style.display = 'none';
 
-function ageCheck(age){
-    ageEl.style.display = "block";
+function ageCheck(){
     age = ageObject.value;
     if(age >= 21){
-        start.style.display = "inline";
-        newCardObj.style.display = "inline";
-        ageMsg = "Welcome to the Club. Enjoy your visit 😊"
+        messageEl.textContent = messages[0];
+        start.style.display = 'block';
     }
-    else if(age > 0 &&  age < 21){
-        cardsEl.style.display = "none";
-        sumEl.style.display = "none";
-        start.style.display = "none";
-        newCardObj.style.display = "none";
-        ageMsg = "We're sorry. You're not allowed to enter this club 🥲"
+    else if(age>0 && age<21){
+        messageEl.textContent = messages[1];
     }
     else{
-        ageMsg = "Inavlid Input. Please try again"
-    }    
-    ageEl.textContent = ageMsg;
-    cardsEl.textContent = "Cards: ";
-    sumEl.textContent = "Sum: ";
-}
-
-function checkHand(sum){
-    if (sum === 21){
-        handState = true;
-        message = "Congratulations, you won."
+        messageEl.textContent = messages[2];
     }
-    else if(sum > 21){
-        isAlive = false;
-        message = "You've losen your hand.";
-    }
-    else{
-        message = "You've got one more chance.";
-    }
-}
-
-function newCard(){
-    let random1 = Math.random();
-    let random = Math.random();
-    firstCard = Math.floor((random1 * 21)+1);
-    secondCard = Math.floor((random * 21)+1);
-    cardsEl.textContent = "Cards: "+ firstCard + " + " + secondCard;
-    sumCards(firstCard, secondCard);
-    checkHand(sum);
-    messageEl.textContent = message;
-    cardsEl.textContent += "";
-    sumEl.textContent = "Sum: " + sum;
 }
 
 function startGame(){
-    newCard();
-    cardsEl.style.display = "block";
-    sumEl.style.display = "block";
-    newCardObj.style.display = "block"
-    ageEl.style.display = "none";
-    start.style.display = 'none';
+    newCardObj.style.display = 'block';
+}
+
+function newCard(){
+    sumEl.style.display = 'block';
+    cardsEl.style.display = 'block';
 }
